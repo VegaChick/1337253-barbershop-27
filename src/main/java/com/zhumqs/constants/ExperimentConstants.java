0feaeea3fe0e252260cbc3cc394b947820e8e3b1
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-05 12:32:58 +0000

    drm sis: initialize object_idr

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -6,4 +6,4 @@ package com.zhumqs.constants;
  */
 public class ExperimentConstants {
     public static final int CONTENT_DEFAULT_SIZE = 1024;
-    public static fin
+    public static final int DEVICE_CAPACITY = 
