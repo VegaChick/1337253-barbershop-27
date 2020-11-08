e1474e4c9928a4cfd840f1f8f97b06a22195e5b9
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-08 12:21:16 +0000

    drafts

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -7,4 +7,5 @@ package com.zhumqs.constants;
 public class ExperimentConstants {
     public static final int CONTENT_DEFAULT_SIZE = 1024;
     public static final int DEVICE_CAPACITY = 5;
-    public static final double DEFAULT_WEIGHT1 =
+    public static final double DEFAULT_WEIGHT1 = 0.5;
+    public sta
