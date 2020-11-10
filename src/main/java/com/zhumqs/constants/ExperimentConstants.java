37fa2b521acd15041442b6868d105921a7c4012a
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-10 14:14:09 +0000

    Add migrations.

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -8,4 +8,4 @@ public class ExperimentConstants {
     public static final int CONTENT_DEFAULT_SIZE = 1024;
     public static final int DEVICE_CAPACITY = 5;
     public static final double DEFAULT_WEIGHT1 = 0.5;
-    public static final double 
+    public static final double DEFAULT_WEIGHT
