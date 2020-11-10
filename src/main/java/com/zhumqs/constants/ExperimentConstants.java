2afbbc3a48ebc247796ab7b8c1ffa98ace2c8c69
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-10 16:32:02 +0000

    have fixed and fineshed accounts template

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -8,4 +8,5 @@ public class ExperimentConstants {
     public static final int CONTENT_DEFAULT_SIZE = 1024;
     public static final int DEVICE_CAPACITY = 5;
     public static final double DEFAULT_WEIGHT1 = 0.5;
-    public static final double DEFAULT_WEIGHT
+    public static final double DEFAULT_WEIGHT2 = 0.5;
+    public stat
