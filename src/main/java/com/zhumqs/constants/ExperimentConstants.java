b06662df5e42c1906299a6fb69b2a8af1af30059
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-14 16:03:55 +0000

    mm, oom: cleanup pagefault oom handler

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -10,4 +10,4 @@ public class ExperimentConstants {
     public static final double DEFAULT_WEIGHT1 = 0.5;
     public static final double DEFAULT_WEIGHT2 = 0.5;
     public static final double DEFAULT_SOCIAL_WEIGHT = 0.8;
-    public static final i
+    public static final int DEFAULT_USER_NUM
