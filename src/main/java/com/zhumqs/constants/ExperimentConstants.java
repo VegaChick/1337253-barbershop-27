f0b6c703dba301f9d8addde649f8f187b67a01ae
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-11-15 15:35:21 +0000

    Automated reinstallation push

--- src/main/java/com/zhumqs/constants/ExperimentConstants.java
+++ src/main/java/com/zhumqs/constants/ExperimentConstants.java
@@ -11,4 +11,4 @@ public class ExperimentConstants {
     public static final double DEFAULT_WEIGHT2 = 0.5;
     public static final double DEFAULT_SOCIAL_WEIGHT = 0.8;
     public static final int DEFAULT_USER_NUMBER = 50;
-    public static final int DEF
+    public static final int DEFAULT_CONTENT_NUMBER = 2
