a272da095a0cdae24d84707c614a141faec123e5
VegaChick <92683603+VegaChick@users.noreply.github.com>
2020-12-13 14:12:04 +0000

    daily

--- src/main/java/com/zhumqs/model/MobileUser.java
+++ src/main/java/com/zhumqs/model/MobileUser.java
@@ -9,3 +9,13 @@ import java.util.List;
  * @author mingqizhu
  * @date 20200102
  */
+@Data
+public class MobileUser {
+    private Integer userId;
+    private Integer age; // 20-50
+    private Integer sex;
+    private Integer institute;
+    private Integer city;
+    private Integer country;
+    private List<Integer> interests;
+}
