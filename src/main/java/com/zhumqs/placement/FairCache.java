95f548bf387eb8846f280da21f46de39086dd976
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-03-19 10:09:28 +0000

    Version bump to 1.8.0

--- src/main/java/com/zhumqs/placement/FairCache.java
+++ src/main/java/com/zhumqs/placement/FairCache.java
@@ -44,4 +44,4 @@ public class FairCache {
 
     private void initCacheStrategy() {
         List<Content> sortedContents = contents.stream().sorted((u1, u2)
-                -> u2.getPopularity().compareTo(u1.getPopularity
+                -> u2.getPopularity().compareTo(u1.getPopularity())).collect(C
