2b16953822b42ee592e56b08cf804fb476046c44
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-06-09 06:50:23 +0000

    v0.4

--- src/main/java/com/zhumqs/placement/FairCache.java
+++ src/main/java/com/zhumqs/placement/FairCache.java
@@ -83,4 +83,5 @@ public class FairCache {
                 if (cachedContentIds.contains(j + 1)) {
                     // 计算与其他缓存用户的平均遭遇概率
                     double totalEncounter = 0.0;
-                    for (int k = 0; k < userNu
+                    for (int k = 0; k < userNumber; k++) {
+     
