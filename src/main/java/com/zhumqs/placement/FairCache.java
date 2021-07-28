74b995abf0b9b1d5d3b26b492917a8e9c68b8b63
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-07-28 20:30:16 +0000

    chore(package): update eslint to version 3.1.1

--- src/main/java/com/zhumqs/placement/FairCache.java
+++ src/main/java/com/zhumqs/placement/FairCache.java
@@ -103,4 +103,4 @@ public class FairCache {
         int[][] trustMat = DataMockUtils.mockTrustRelationship(ExperimentConstants.DEFAULT_SOCIAL_WEIGHT, ExperimentConstants.DEFAULT_USER_NUMBER);
         FairCache fairCache = new FairCache(users, contents, ExperimentConstants.DEVICE_CAPACITY, trustMat);
         double cacheHitRatio = fairCache.getCacheHitRatio();
-        log
+        log.info("Cache hit r
