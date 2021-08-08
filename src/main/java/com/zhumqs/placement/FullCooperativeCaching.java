63746e5502fe75acd049633822345f8dbc41dd68
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-08-08 17:06:07 +0000

    Some refactors

--- src/main/java/com/zhumqs/placement/FullCooperativeCaching.java
+++ src/main/java/com/zhumqs/placement/FullCooperativeCaching.java
@@ -22,3 +22,22 @@ public class FullCooperativeCaching {
 
     private double weight1;
     private double weight2;
+    private int userNumber;
+    private int contentNumber;
+    private int capacity;
+    private List<MobileUser> users;
+    private List<Content> contents;
+    private int[][] trustMat;
+
+    private double[][] encounterMat;
+    private double[][] requestProbabilityMat;
+    private double[][] expectationMat;
+    private Map<Integer, List<Integer>> cacheMap;
+    private Map<Integer, Integer> placementMap;
+
+    public FullCooperativeCaching(double weight1, double weight2, int capacity,
+                                  List<MobileUser> users,
+                                  List<Content> contents,
+                                  int[][] trustMat) {
+        this.weight1 = weight1;
+        this.weight2 = weight2;
