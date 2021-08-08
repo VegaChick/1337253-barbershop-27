79270bdb30d92f5e4dfaa5a1028e137d6e0c2bd0
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-08-08 15:11:35 +0000

    ceph: ceph_pagelist_append might sleep while atomic

--- src/main/java/com/zhumqs/placement/FullCooperativeCaching.java
+++ src/main/java/com/zhumqs/placement/FullCooperativeCaching.java
@@ -1,2 +1,24 @@
 
 package com.zhumqs.placement;
+
+import com.zhumqs.constants.ExperimentConstants;
+import com.zhumqs.encounter.EncounterProbability;
+import com.zhumqs.model.Content;
+import com.zhumqs.model.MobileUser;
+import com.zhumqs.predict.RequestProbability;
+import com.zhumqs.utils.DataMockUtils;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+/**
+ * @author mingqizhu
+ * @date 20200103
+ */
+@Slf4j
+@Data
+public class FullCooperativeCaching {
+
+    private double weight1;
+    private double weight2;
