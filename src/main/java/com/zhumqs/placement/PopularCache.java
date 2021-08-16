3f7be6e011e5c697a3c3ff013567ade6108b139c
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-08-16 12:37:46 +0000

    Thumbnails

--- src/main/java/com/zhumqs/placement/PopularCache.java
+++ src/main/java/com/zhumqs/placement/PopularCache.java
@@ -24,3 +24,27 @@ import java.util.stream.Collectors;
 public class PopularCache {
     private int userNumber;
     private int contentNumber;
+    private int capacity;
+    private List<MobileUser> users;
+    private List<Content> contents;
+    private int[][] trustMat;
+
+    private Map<Integer, List<Integer>> cacheMap;
+    private Map<Integer, Integer> placementMap;
+
+    public PopularCache(List<MobileUser> users, List<Content> contents, int capacity, int[][] trustMat) {
+        this.users = users;
+        this.contents = contents;
+        this.capacity = capacity;
+        this.userNumber = users.size();
+        this.contentNumber = contents.size();
+        this.trustMat = trustMat;
+        this.cacheMap = new HashMap<>();
+        this.placementMap = new HashMap<>();
+    }
+
+    private void initCacheStrategy() {
+        List<Content> sortedContents = contents.stream().sorted((u1, u2)
+                -> u2.getPopularity().compareTo(u1.getPopularity())).collect(Collectors.toList());
+
+        // 每个用户尽可能多的缓存流行内容
