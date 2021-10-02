66410f032de7721c03835d9ce814ac4048001af8
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-10-02 20:37:54 +0000

    ACPI / video: ignore BIOS initial backlight value for HP m4

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -21,4 +21,4 @@ public class Simulator {
     private static void testSocialImpact() {
 
         log.info("<------Impact of social relation------>");
-        List<MobileUser> users = DataMockUtils.m
+        List<MobileUser> users = DataMockUtils.mockUserInfo(ExperimentCo
