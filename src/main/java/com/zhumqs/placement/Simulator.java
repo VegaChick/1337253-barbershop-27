059af8c6a148bec01f2512a9f255524cd89d0e66
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-11-30 19:01:06 +0000

    add ruby

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -36,4 +36,4 @@ public class Simulator {
             FullCooperativeCaching fullCooperativeCaching = new FullCooperativeCaching(ExperimentConstants.DEFAULT_WEIGHT1,
                     ExperimentConstants.DEFAULT_WEIGHT2,
                     ExperimentConstants.DEVICE_CAPACITY,
-                    
+                    users, contents,
