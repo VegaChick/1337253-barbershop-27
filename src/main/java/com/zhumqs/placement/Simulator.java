cc30ec4e9858b5b2a609548b706874cca189a045
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-12-10 22:44:55 +0000

    bump to 0.5.0

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -39,4 +39,4 @@ public class Simulator {
                     users, contents, trustMat);
             double d1 = fullCooperativeCaching.getCacheHitRatio();
             cacheHitRatioForFullCooperation.add(d1);
-            log.info("ProposedCaching: Social relation: {}, cach
+            log.info("ProposedCaching: Social relation: {}, cache hit ratio: {} ", so
