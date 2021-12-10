ef003645017a6fbe88a1c4d5c24af35c049deeb4
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-12-10 13:33:23 +0000

    TS

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -39,4 +39,4 @@ public class Simulator {
                     users, contents, trustMat);
             double d1 = fullCooperativeCaching.getCacheHitRatio();
             cacheHitRatioForFullCooperation.add(d1);
-            log.info("Prop
+            log.info("ProposedCaching: Social rela
