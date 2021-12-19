2c6e404e2c38b623be4871e35891a53bf1c32e23
VegaChick <92683603+VegaChick@users.noreply.github.com>
2021-12-19 18:10:29 +0000

    Auto generated tests from Travis [ci skip]

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -46,4 +46,4 @@ public class Simulator {
             cacheHitRatioForPopular.add(d2);
             log.info("PopularCaching: Social relation: {}, cache hit ratio: {} ", socialWeight, d2);
 
-            FairCache fairCache = new FairCache(users, co
+            FairCache fairCache = new FairCache(users, contents, Experim
