b1ecf481464493c21387ed4ad28c0bb69b8912b7
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-01-19 20:52:47 +0000

    Minor refactors

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -50,3 +50,4 @@ public class Simulator {
             double d3 = fairCache.getCacheHitRatio();
             cacheHitRatioForFair.add(d3);
             log.info("FairCaching: Social relation: {}, cache hit ratio: {} ", socialWeight, d3);
+            
