36cef3370fb0b8458dacbe190adaffd1d2d26015
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-02-15 18:19:01 +0000

    mi

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -56,4 +56,4 @@ public class Simulator {
 
         log.info("ProposedCache: {}", cacheHitRatioForFullCooperation.toString());
         log.info("PopularCache: {}", cacheHitRatioForPopular.toString());
-       
+        log.info("FairCache: {}", c
