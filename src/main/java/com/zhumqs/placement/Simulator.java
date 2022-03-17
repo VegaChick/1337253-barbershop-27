e19c7c4aaf480a0bf20cc6cebe326b979d3d6bf0
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-03-17 07:55:30 +0000

    Added my username

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -70,4 +70,5 @@ public class Simulator {
         List<Double> cacheHitRatioForFair = new ArrayList<>();
 
         for (int i = 10; i <= 100; i += 10) {
-            List<MobileUser> users = DataMockUtils.mockU
+            List<MobileUser> users = DataMockUtils.mockUserInfo(i);
+      
