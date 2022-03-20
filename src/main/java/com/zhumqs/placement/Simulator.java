351320beadf3e12acf53aec29730f83c74a6e9b9
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-03-20 13:38:47 +0000

    fix some bug

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -71,4 +71,4 @@ public class Simulator {
 
         for (int i = 10; i <= 100; i += 10) {
             List<MobileUser> users = DataMockUtils.mockUserInfo(i);
-            int[][] trustMat = DataMockUtils.mockTrustRelationship(ExperimentConsta
+            int[][] trustMat = DataMockUtils.mockTrustRelationship(ExperimentConstants.DEFAULT_SOCIAL_W
