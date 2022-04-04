bf0f2699739abd155cdae08a1b3e67d17e32e676
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-04-04 21:17:22 +0000

    Update german translations

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -74,4 +74,5 @@ public class Simulator {
             int[][] trustMat = DataMockUtils.mockTrustRelationship(ExperimentConstants.DEFAULT_SOCIAL_WEIGHT, i);
             FullCooperativeCaching fullCooperativeCaching = new FullCooperativeCaching(ExperimentConstants.DEFAULT_WEIGHT1,
                     ExperimentConstants.DEFAULT_WEIGHT2,
-                    ExperimentConstants.DEVICE_CAPACITY
+                    ExperimentConstants.DEVICE_CAPACITY,
+                    
