22cf6aab28a8cc85c5d89aa51ad4d692f898e2c2
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-04-13 12:36:07 +0000

    Update influxdata images

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -78,4 +78,4 @@ public class Simulator {
                     users, contents, trustMat);
             double d1 = fullCooperativeCaching.getCacheHitRatio();
             cacheHitRatioForFullCooperation.add(d1);
-            log.info("ProposedCaching: User number: {}, cac
+            log.info("ProposedCaching: User number: {}, cache hit ratio: {} ", i, d1)
