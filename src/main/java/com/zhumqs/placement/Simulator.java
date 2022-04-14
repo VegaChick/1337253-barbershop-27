08504ac51451292c11c3f4b70a357041a6361324
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-04-14 13:35:01 +0000

    * Added test code of the menu tag for XHTML1.0 converter.

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -80,4 +80,4 @@ public class Simulator {
             cacheHitRatioForFullCooperation.add(d1);
             log.info("ProposedCaching: User number: {}, cache hit ratio: {} ", i, d1);
 
-            PopularCache po
+            PopularCache popularCache = new PopularC
