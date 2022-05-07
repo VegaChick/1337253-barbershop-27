5f929e1b50d37944e7c95ccfe8bb7cb85feb3fbb
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-05-07 22:32:52 +0000

    update badges

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -86,4 +86,4 @@ public class Simulator {
             log.info("PopularCaching: User number: {}, cache hit ratio: {} ", i, d2);
 
             FairCache fairCache = new FairCache(users, contents, ExperimentConstants.DEVICE_CAPACITY, trustMat);
-            double d
+            double d3 = fairCache.getCache
