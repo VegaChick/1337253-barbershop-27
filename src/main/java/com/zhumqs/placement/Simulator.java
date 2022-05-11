b9bad73e85ff6dff93d4e4236f7760f7d7ac584f
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-05-11 22:45:32 +0000

    Correct copyright notice

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -88,3 +88,4 @@ public class Simulator {
             FairCache fairCache = new FairCache(users, contents, ExperimentConstants.DEVICE_CAPACITY, trustMat);
             double d3 = fairCache.getCacheHitRatio();
             cacheHitRatioForFair.add(d3);
+            log.info
