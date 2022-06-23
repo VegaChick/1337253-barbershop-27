6a028657db9367ff819a433a0a307a385d57b6b6
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-06-23 20:59:53 +0000

    original

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -105,4 +105,4 @@ public class Simulator {
 
         List<Double> cacheHitRatioForFullCooperation = new ArrayList<>();
         List<Double> cacheHitRatioForPopular = new ArrayList<>();
-    
+        List<Double> cache
