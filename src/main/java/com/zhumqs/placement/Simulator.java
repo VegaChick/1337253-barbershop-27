2c23027a926a43d52e27c738c050414137e59f18
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-07-19 16:09:15 +0000

    Factorize code

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -115,4 +115,4 @@ public class Simulator {
                     ExperimentConstants.DEVICE_CAPACITY,
                     users, contents, trustMat);
             double d1 = fullCooperativeCaching.getCacheHitRatio();
-            cach
+            cacheHitRatioForFullCooperation
