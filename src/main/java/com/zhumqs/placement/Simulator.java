63e7027d188761f0dfa3573e87fc18f7ad41c4cc
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-08-03 18:34:55 +0000

    Initial create

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -121,4 +121,4 @@ public class Simulator {
             PopularCache popularCache = new PopularCache(users, contents, ExperimentConstants.DEVICE_CAPACITY, trustMat);
             double d2 = popularCache.getCacheHitRatio();
             cacheHitRatioForPopular.add(d2);
-            lo
+            log.info("PopularCachi
