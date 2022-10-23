a32029764b8bf471de893581763b56d0685d927b
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-10-23 08:58:15 +0000

    responsive

--- src/main/java/com/zhumqs/placement/Simulator.java
+++ src/main/java/com/zhumqs/placement/Simulator.java
@@ -150,4 +150,4 @@ public class Simulator {
             log.info("ProposedCaching: Capacity: {}, cache hit ratio: {} ", i, fullCooperativeCaching.getCacheHitRatio());
 
             PopularCache popularCache = new PopularCache(users, contents, ExperimentConstants.DEVICE_CAPACITY, trustMat);
-            log.info("PopularCaching: Capacity: {}, cache hit ratio: {} ", i, popula
+            log.info("PopularCaching: Capacity: {}, cache hit ratio: {} ", i, popularCache.getCacheHitRatio());
