b455b292c0383c43d27b5238237078d8758ee0dd
VegaChick <92683603+VegaChick@users.noreply.github.com>
2022-12-25 18:24:49 +0000

    Refatoração.

--- src/main/java/com/zhumqs/predict/RequestProbability.java
+++ src/main/java/com/zhumqs/predict/RequestProbability.java
@@ -25,4 +25,4 @@ public class RequestProbability {
     public double[][] getRequestProbabilityMatrix() {
         int userNumber = users.size();
         int contentNumber = contents.size();
-        double[][] requestProbabilityMat = ne
+        double[][] requestProbabilityMat = new double[userNu
