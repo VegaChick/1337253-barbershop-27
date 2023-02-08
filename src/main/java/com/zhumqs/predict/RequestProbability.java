c59569d21362a38078865c234b773e2ec5807c55
VegaChick <92683603+VegaChick@users.noreply.github.com>
2023-02-08 13:26:03 +0000

    Рефакторинг

--- src/main/java/com/zhumqs/predict/RequestProbability.java
+++ src/main/java/com/zhumqs/predict/RequestProbability.java
@@ -36,4 +36,4 @@ public class RequestProbability {
 
     public static void main(String[] args) {
         RequestProbability probability = new RequestProbability(DataMockUtils.mockUserInfo(100), DataMockUtils.mockContents(500));
-        do
+        double[][] requestP
