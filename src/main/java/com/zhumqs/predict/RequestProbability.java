0b59cd3d2a5030396bc04ee6afd1b4da01455e2d
VegaChick <92683603+VegaChick@users.noreply.github.com>
2023-02-20 09:29:00 +0000

    Win32: make FILETIME conversion functions public

--- src/main/java/com/zhumqs/predict/RequestProbability.java
+++ src/main/java/com/zhumqs/predict/RequestProbability.java
@@ -37,4 +37,4 @@ public class RequestProbability {
     public static void main(String[] args) {
         RequestProbability probability = new RequestProbability(DataMockUtils.mockUserInfo(100), DataMockUtils.mockContents(500));
         double[][] requestProbabilityMatrix = probability.getRequestProbabilityMatrix();
-   
+        log.info(Arrays.d
